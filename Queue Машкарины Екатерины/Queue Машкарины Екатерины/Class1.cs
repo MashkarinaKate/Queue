3c39018679920d9da87e9ddc516b02d6aecc3ad5
MashkarinaKate <mashkarinakate@gmail.com>
2020-11-24 21:02:31 +0300

    очередь

---
+++ Queue Машкарины Екатерины/Queue Машкарины Екатерины/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Машкарины_Екатерины
+{
+    public class Queue<T>
+    {
+        public T[] array;
+        int head;
+        int tail;
+        public int size;
+        int capacity;
+        T f;
+        public Queue()
+        {
+            size = 0;
+            head = 0;
+            tail = -1;
+            array = new T[1];
+            capacity = 1;
+        }
+        bool isEmpty()
+        {
+            if (size == 0) return true;
+            else return false;
+        }
+        public void Enqueue(T item)
+        {
+            if (item == null)
+            {
+                throw new Exception();
+            }
+            tail++;
+            size++;
+            capacity++;
+            T[] array2 = new T[capacity];
+            Array.Copy(array, array2, tail);
+            array2[tail] = item;
+            array = new T[size];
+            Array.Copy(array2, array, size);
+        }
+        public T Dequeue()
+        {
+            if (!isEmpty())
+            {
+                head++;
+                size--;
+                capacity--;
+                return array[head];
+            }
+            else return f;
+        }
+        public T Peek()
+        {
+            if (!isEmpty())
+                return array[tail];
+            else return f;
+        }
+        public void Clear()
+        {
+            array = new T[1];
+            head = 0;
+            size = 0;
+        }
+        public int Count
+        {
+            get { return size; }
+        }
+    }
+}
+
---
+++ Queue Машкарины Екатерины/Queue Машкарины Екатерины/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Машкарины_Екатерины
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(1);
+            q.Enqueue(3);
+            q.Enqueue(10);
+            for(int i = 0; i < q.size; i++)
+            {
+                Console.Write(q.array[i] + " ");
+            }
+            Console.WriteLine("\n"+ q.Peek());
+            q.Enqueue(5);
+            for (int i = 0; i < q.size; i++)
+            {
+                Console.Write(q.array[i] + " ");
+            }
+            Console.WriteLine();
+            q.Dequeue();
+            for (int i = 0; i < q.size; i++)
+            {
+                Console.Write(q.array[i] + " ");
+            }
+            q.Clear();
+            Console.WriteLine("/");
+            Console.WriteLine(q.Dequeue());
+            for (int i = 0; i < q.size; i++)
+            {
+                Console.Write(q.array[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
